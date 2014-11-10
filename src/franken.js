65daf416a7bab6c7968e0c915632f50470b0c8e7
Athan Clark <athan.clark@gmail.com>
2014-11-10 13:00:52 -0700

    refactored utilities out of main file, will use r.js ...later

--- src/franken.js
+++ src/franken.js
@@ -1,3 +1,7 @@
+var utils = require('./utils.js');
+var zipWith = utils.zipWith;
+var app = utils.app;
+
 function clone(obj) {
 	if (null == obj || "object" != typeof obj) return obj;
 	var copy = obj.constructor();
@@ -31,38 +35,4 @@ function Ughh(raw){
 		}
 	}
 	return func;
-}
-
-function zipWith(f, a, b){
-	var result = []
-	function goL(elem, index, array){
-		result[index] = f(elem)(b[index]);
-	}
-	function goR(elem, index, array){
-		result[index] = f(a[index])(elem);
-	}
-	if (a.length <= b.length) {
-		a.forEach(goL);
-		return result;
-	} else {
-		b.forEach(goR);
-		return result;
-	}
-}
-
-function app(fs, a){
-	var result = [];
-	function iterateeFromSelf(elem, index, array){
-		result[index] = elem(a[index]);
-	}
-	function iterateeFromOther(elem, index, array){
-		result[index] = fs[index](elem);
-	}
-	if (fs.length <= a.length) {
-		fs.forEach(iterateeFromSelf);
-		return result;
-	} else {
-		a.forEach(iterateeFromOther);
-		return result;
-	}
 }
